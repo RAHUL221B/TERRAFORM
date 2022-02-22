c7b93df249b79d65d235f0ab48e423297aba0835
rahul <rrpatil444@gmail.com>
2022-02-23 02:59:38 +0530

    Added changest to count

--- activity2/network.tf
+++ activity2/network.tf
@@ -1,10 +1,17 @@
+resource "aws_vpc" "primary_vpc" {
+  cidr_block = "192.168.0.0/16"
+  tags = {
+      Name = "primary"
+  }
+}
+
+
 resource "aws_subnet" "subnets" {
   count = length(var.subnet_cidrs)
 
   vpc_id = aws_vpc.primary_vpc.id
   availability_zone = var.subnet_azs[count.index]
   cidr_block = var.subnet_cidrs[count.index]
-
 
   tags = {
       Name = var.subnet_names[count.index]
